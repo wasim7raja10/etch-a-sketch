4af2cbc6a18661d7da1b7be27545d7dbb6b7953e
Wasim Raja <wasim7raja10@gmail.com>
2021-10-05 17:33:12 +0530

    fixed variable number of side grid making

--- js/app.js
+++ js/app.js
@@ -1,26 +1,43 @@
 const container = document.querySelector('.container');
 const clrBtn = document.querySelector('.clear');
+const numberSide = 16
 
-for (let i = 0; i < 16; i++) {
-  for (let j = 0; j < 16; j++) {
-    const square = document.createElement("div");
-    square.setAttribute(
-      "style", "width: 2rem; height: 2rem; display: inline-block"
-    );
-    container.appendChild(square);
+function makeGrid(numberSide) {
+  for (let i = 0; i < numberSide; i++) {
+    for (let j = 0; j < numberSide; j++) {
+      const square = document.createElement("div");
+      square.setAttribute(
+        "style", `width: ${32/numberSide}rem; height: ${32/numberSide}rem; display: inline-block`
+      );
+      container.appendChild(square);
+    }
   }
 }
 
-container.childNodes.forEach((x) => {
-  const mark = () => x.style.background = "black";
-  x.addEventListener('mouseenter', mark);
-})
+function markSquare() {
+  container.childNodes.forEach((x) => {
+    const mark = () => x.style.background = "black";
+    x.addEventListener('mouseenter', mark);
+  })
+}
 
-function clear() {
+function removeMark() {
   container.childNodes.forEach((x) => {
     x.style.background = null;
   })
-  const numberSide = parseInt(prompt("enter number of square in each side"));
+}
+
+makeGrid(numberSide);
+markSquare();
+
+function clear() {
+  removeMark();
+  const numberSide = parseInt(
+    prompt("enter number of square in each side")
+  );
+  container.innerHTML = null;
+  makeGrid(numberSide);
+  markSquare();
 }
 
 clrBtn.addEventListener('click', clear);
